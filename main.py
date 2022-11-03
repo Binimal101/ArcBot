55f9953c50673cce964cb05d0bcdb32c72ff3ad8
Matthew Tujague <81255678+Binimal101@users.noreply.github.com>
2022-11-03 03:48:55 +0000

    Comment

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ activity = discord.Activity(type=discord.ActivityType.listening, name="the typin
 permissions = discord.Permissions(administrator=True)
 client = discord.Client(intents=intents,activity=activity,permissions=permissions) #Creates bot cli with intents; just means the bot has special permissions
 
-
 @client.event #this just means that this function is passed into discords client object as a wrapper for the event superclass
 async def on_ready():
 	os.system('clear') #clears terminal for aesthetics
@@ -23,7 +22,7 @@ async def on_ready():
 @client.event 
 async def on_message(message): #message object is given to this function
 	if message.author.bot or message.author.id == client.user.id or isinstance(message.channel, discord.channel.DMChannel): #just checks to see if the bot is talking to itself or if it's talking in a dm
-		return #just so the thread doesn't sit endlessly and crash
+		return #just so the thread doesn't sit idly
 	
 	connection = message.channel #this var has a instance function called send that sends messages and embeds to the channel of the message instance
 	guild = connection.guild
@@ -38,6 +37,10 @@ async def on_message(message): #message object is given to this function
 
 		await warning.delete() #delete the warning
 
+	
+ 	# each command is called when all of the aliases (string arguements in commands.command) are called in a message in addition to the bot name,
+    # there is also the option to run when any of the aliases are in the message, which will be checked off when or_aliases is set to true
+ 	
 	elif commands.command(message, 'hey', 'hi', 'howdy', 'hello', 'heya', or_aliases=True):
 		await Fun.greet(message)
 	
@@ -87,7 +90,5 @@ async def on_message(message): #message object is given to this function
 	elif commands.command(message, 'inv', 'inventory', or_aliases=True):
 		embed = await database.commands.inventory(client, message.author)
 		await message.channel.send(embed=embed)
-
-	
 
 client.run(TOKEN)
